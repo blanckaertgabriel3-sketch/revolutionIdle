1ae3c86cdfc76b4bacc0ece182baced785fe8a84
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-02-11 20:03:11 +0100

    end html part

--- frontend/Model.js
+++ frontend/Model.js
@@ -1,5 +1,15 @@
 export default class Model {
 	constructor() {
 		this.multiplierA = 0.00;
+		this.multiplierB = 0.00;
+		this.multiplierC = 0.00;
+		this.multiplierD = 0.00;
+		this.multiplierE = 0.00;
+		this.multiplierF = 0.00;
+		this.multiplierG = 0.00;
+		this.multiplierH = 0.00;
+		this.multiplierI = 0.00;
+		this.multiplierJ = 0.00;
+		
 	}
 }
--- frontend/View.js
+++ frontend/View.js
@@ -8,12 +8,33 @@ export default class View {
 		//multipliers
 		this.multipliers = document.getElementById("multipliers");
 		this.multiplierA = document.getElementById("multiplierA");
+		this.multiplierB = document.getElementById("multiplierB");
+		this.multiplierC = document.getElementById("multiplierC");
+		this.multiplierD = document.getElementById("multiplierD");
+		this.multiplierE = document.getElementById("multiplierE");
+		this.multiplierF = document.getElementById("multiplierF");
+		this.multiplierG = document.getElementById("multiplierG");
+		this.multiplierH = document.getElementById("multiplierH");
+		this.multiplierI = document.getElementById("multiplierI");
+		this.multiplierJ = document.getElementById("multiplierJ");
 		//buttons
 		this.upgradeSpeedBtnA = document.getElementById("upgradeSpeedBtnA");
 		this.upgradeSpeedBtnB = document.getElementById("upgradeSpeedBtnB");
 	}
 	updateView() {
 		this.multiplierA.innerHTML = this.model.multiplierA;
+
+
+		this.multiplierA.innerHTML = this.model.multiplierA.toFixed(2);
+		this.multiplierB.innerHTML = this.model.multiplierB.toFixed(2);
+		this.multiplierC.innerHTML = this.model.multiplierC.toFixed(2);
+		this.multiplierD.innerHTML = this.model.multiplierD.toFixed(2);
+		this.multiplierE.innerHTML = this.model.multiplierE.toFixed(2);
+		this.multiplierF.innerHTML = this.model.multiplierF.toFixed(2);
+		this.multiplierG.innerHTML = this.model.multiplierG.toFixed(2);
+		this.multiplierH.innerHTML = this.model.multiplierH.toFixed(2);
+		this.multiplierI.innerHTML = this.model.multiplierI.toFixed(2);
+		this.multiplierJ.innerHTML = this.model.multiplierJ.toFixed(2);
 	}
 
 }
