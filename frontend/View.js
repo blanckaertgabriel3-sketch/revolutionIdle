5e18d311ae6f0c35a1200ad624b715dbfe5672e0
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-02-14 14:39:05 +0100

    automaticaly collor buttons and multipliers, optimize code

--- frontend/View.js
+++ frontend/View.js
@@ -4,6 +4,8 @@ export default class View {
 		//canvas
 		this.canvas = document.getElementById("canvas");
 		this.ctx = this.canvas.getContext("2d");
+
+
 		this.color = [
 			"#E64F4F","#FFA64D","#F8F84A","#A0F549","#4BFEA4","#4CFFFF","#3F3FD4","#9F49F4","#FB4BFB","#FFFFFF"
 		];
@@ -11,55 +13,27 @@ export default class View {
 			"A","B","C","D","E","F","G","H","I","J",
 		]
 
-		//multipliers
-		this.multipliers = document.getElementById("multipliers");
-		this.multiplierA = document.getElementById("multiplierA");
-		this.multiplierB = document.getElementById("multiplierB");
-		this.multiplierC = document.getElementById("multiplierC");
-		this.multiplierD = document.getElementById("multiplierD");
-		this.multiplierE = document.getElementById("multiplierE");
-		this.multiplierF = document.getElementById("multiplierF");
-		this.multiplierG = document.getElementById("multiplierG");
-		this.multiplierH = document.getElementById("multiplierH");
-		this.multiplierI = document.getElementById("multiplierI");
-		this.multiplierJ = document.getElementById("multiplierJ");
-		//buttons
-		this.upgradeSpeedBtnA = document.getElementById("upgradeSpeedBtnA");
-		this.upgradeSpeedBtnB = document.getElementById("upgradeSpeedBtnB");
-		this.upgradeSpeedBtnC = document.getElementById("upgradeSpeedBtnC");
-		this.upgradeSpeedBtnD = document.getElementById("upgradeSpeedBtnD");
-		this.upgradeSpeedBtnE = document.getElementById("upgradeSpeedBtnE");
-		this.upgradeSpeedBtnF = document.getElementById("upgradeSpeedBtnF");
-		this.upgradeSpeedBtnG = document.getElementById("upgradeSpeedBtnG");
-		this.upgradeSpeedBtnH = document.getElementById("upgradeSpeedBtnH");
-		this.upgradeSpeedBtnI = document.getElementById("upgradeSpeedBtnI");
-		this.upgradeSpeedBtnJ = document.getElementById("upgradeSpeedBtnJ");
-
+		//get multiplers, buttons By Id
+		for(let i=0; i<this.letters.length; i++) {
+			//multipliers
+			this["multiplier" + this.letters[i]] = document.getElementById("multiplier" + this.letters[i]);
+			//buttons
+			this["upgradeSpeedBtn" + this.letters[i]] = document.getElementById("upgradeSpeedBtn" + this.letters[i]);
+		}
+		//colors: multipliers, buttons
+		for(let i=0; i<this.letters.length; i++) {
+			this["multiplier" + this.letters[i]].style.color = this.color[i];
+			this["upgradeSpeedBtn" + this.letters[i]].style.backgroundColor = this.color[i];
+		}
 		//available funds 
 		this.availableFunds = document.getElementById("availableFunds");
-
-		//buttons color and multipliers color
-		//generate the path
-		for(let i=0; i<this.letters.length; i++) {
-			console.log("i", i);
-			let multiplierPath = "multiplier" + this.letters[i];
-			console.log("path:",multiplierPath);
-		}
-		//objectif automatise this:
-		this.multiplierA.style.color = this.color[2];
 		
 	}
 	updateView() {
-		this.multiplierA.textContent = this.model.multiplierA.toFixed(2);
-		this.multiplierB.textContent = this.model.multiplierB.toFixed(2);
-		this.multiplierC.textContent = this.model.multiplierC.toFixed(2);
-		this.multiplierD.textContent = this.model.multiplierD.toFixed(2);
-		this.multiplierE.textContent = this.model.multiplierE.toFixed(2);
-		this.multiplierF.textContent = this.model.multiplierF.toFixed(2);
-		this.multiplierG.textContent = this.model.multiplierG.toFixed(2);
-		this.multiplierH.textContent = this.model.multiplierH.toFixed(2);
-		this.multiplierI.textContent = this.model.multiplierI.toFixed(2);
-		this.multiplierJ.textContent = this.model.multiplierJ.toFixed(2);
+		//update all textContent multipliers
+		for(let i=0 ; i<this.letters.length ; i++) {
+			this["multiplier" + this.letters[i]].textContent = this.model["multiplier" + this.letters[i]].toFixed(2);
+		}
 
 		this.availableFunds.innerHTML = this.model.availableFunds.toFixed(2);	
 	}
